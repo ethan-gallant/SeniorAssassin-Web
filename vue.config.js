3f4559f81c795a87ce5b7093314d98da069af8c3
Cameron <github@strixdesigns.com>
2020-01-26 11:57:24 -0800

    fix icon directory issues

--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,19 @@ module.exports = {
             name: 'SeniorAssassin',
             short_name: 'Assassin',
             start_url: '/',
-            display: 'standalone'
+            display: 'standalone',
+            "icons": [
+                {
+                    "src": "./img/icons/android-icon-192x192.png",
+                    "sizes": "192x192",
+                    "type": "image/png"
+                },
+                {
+                    "src": "./img/icons/android-icon-512x512.png",
+                    "sizes": "512x512",
+                    "type": "image/png"
+                }
+            ],
         },
 
         // configure the workbox plugin
