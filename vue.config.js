601337b5202c67a97253a89a4b353d5a629b625d
Cameron <github@strixdesigns.com>
2020-01-26 13:20:39 -0800

    add apple icon

--- vue.config.js
+++ vue.config.js
@@ -22,6 +22,16 @@ module.exports = {
                     "src": "/img/icons/android-icon-512x512.png",
                     "sizes": "512x512",
                     "type": "image/png"
+                },
+                {
+                    "src": "/img/icons/apple-icon-180x180.png",
+                    "sizes": "192x192",
+                    "type": "image/png"
+                },
+                {
+                    "src": "/img/icons/apple-icon.png",
+                    "sizes": "512x512",
+                    "type": "image/png"
                 }
             ],
         },
