6584569481ddd17cde095ab8efa61f90b089f1b3
Cameron <github@strixdesigns.com>
2020-01-25 23:57:08 -0800

    add pwa functionality

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,23 @@
 module.exports = {
     publicPath: './',
+    pwa: {
+        name: 'SeniorAssassin',
+        themeColor: '#4DBA87',
+        msTileColor: '#000000',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black',
+        manifestOptions:{
+            name: 'SeniorAssassin',
+            short_name: 'Assassin',
+            start_url: '.',
+            display: 'standalone'
+        },
+
+        // configure the workbox plugin
+        workboxPluginMode: 'GenerateSW',
+        workboxOptions: {
+            swDest: 'service-worker.js',
+            // ...other Workbox options...
+        }
+    }
 };
