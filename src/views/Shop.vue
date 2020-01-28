f5fcbe2610c21f492d015c2eddacb75b13b286df
Cameron <github@strixdesigns.com>
2020-01-27 20:18:43 -0800

    add empty shop page

---
+++ src/views/Shop.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="has-text-centered shop">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Shop"
+    }
+</script>
+
+<style scoped>
+
+</style>
